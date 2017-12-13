911e0c1d57eaa7f7ddb2420b41c43f80c9c23de1
Robert Daignault <beau@littlehou.se>
2017-12-13 11:47:37 -0700

    Add custom header support

--- header.php
+++ header.php
@@ -23,7 +23,11 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'mtx' ); ?></a>
-
+	<?php if ( get_header_image()) : ?>
+		<figure class="header-image">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php the_header_image_tag(); ?></a>
+		</figure> <!--  header image -->
+	<?php endif; ?>
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
